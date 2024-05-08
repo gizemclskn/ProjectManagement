dad6b7e60b8178ba95380e27516bd9654d8cd009
Gizem Çalışkan <gizemcaliskan24@gmail.com>
2024-05-08 10:42:19 +0300

    Added CustomersController and ProjectsController

---
+++ ProjectManagement/Controllers/CustomersController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UnitOfWorks;
+using ProjectManangment.Model;
+
+namespace ProjectManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CustomersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpPost("list")]
+        public IActionResult ListCustomers()
+        {
+            var customers = _unitOfWork.Customers.GetAll();
+            return Ok(customers);
+        }
+        [HttpPost("detail")]
+        public IActionResult GetCustomer([FromBody] int customerId)
+        {
+            var customer = _unitOfWork.Customers.Get(customerId);
+            return customer != null ? Ok(customer) : NotFound();
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateCustomer([FromBody] Customer customer)
+        {
+            _unitOfWork.Customers.Add(customer);
+            _unitOfWork.Complete();
+            return Created("", customer);
+        }
+        [HttpPost("update")]
+        public IActionResult UpdateCustomer([FromBody] Customer customer)
+        {
+            var existingCustomer = _unitOfWork.Customers.Get(customer.CustomerId);
+            if (existingCustomer == null)
+                return NotFound();
+
+            existingCustomer.Name = customer.Name;
+            existingCustomer.Email = customer.Email;
+            existingCustomer.Phone = customer.Phone;
+            existingCustomer.Address = customer.Address;
+
+            _unitOfWork.Complete();
+            return NoContent();
+        }
+        [HttpPost("delete")]
+        public IActionResult DeleteCustomer([FromBody] int customerId)
+        {
+            var customer = _unitOfWork.Customers.Get(customerId);
+            if (customer == null)
+                return NotFound();
+
+            _unitOfWork.Customers.Remove(customer);
+            _unitOfWork.Complete();
+            return NoContent();
+        }
+    }
+}
---
+++ ProjectManagement/Controllers/ProjectsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UnitOfWorks;
+using ProjectManangment.Model;
+
+namespace ProjectManagement.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProjectsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("list")]
+        public IActionResult ListProjects()
+        {
+            var projects = _unitOfWork.Projects.GetAll();
+            return Ok(projects);
+        }
+        [HttpPost("detail")]
+        public IActionResult GetProject([FromBody] int projectId)
+        {
+            var project = _unitOfWork.Projects.Get(projectId);
+            return project != null ? Ok(project) : NotFound();
+        }
+
+        [HttpPost("create")]
+        public IActionResult CreateProject([FromBody] Project project)
+        {
+            _unitOfWork.Projects.Add(project);
+            _unitOfWork.Complete();
+            return Created("", project);
+        }
+        [HttpPost("update")]
+        public IActionResult UpdateProject([FromBody] Project project)
+        {
+            var existingProject = _unitOfWork.Projects.Get(project.ProjectId);
+            if (existingProject == null)
+                return NotFound();
+
+            existingProject.Name = project.Name;
+            existingProject.StartDate = project.StartDate;
+            existingProject.EndDate = project.EndDate;
+            existingProject.Status = project.Status;
+
+            _unitOfWork.Complete();
+            return NoContent();
+        }
+        [HttpPost("delete")]
+        public IActionResult DeleteProject([FromBody] int projectId)
+        {
+            var project = _unitOfWork.Projects.Get(projectId);
+            if (project == null)
+                return NotFound();
+
+            _unitOfWork.Projects.Remove(project);
+            _unitOfWork.Complete();
+            return NoContent();
+        }
+    }
+}
